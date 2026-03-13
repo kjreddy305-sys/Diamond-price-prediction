7753e6759ee9856827ecd950085972f8269d1d29
komma jayanth reddy <kjreddy305@gmail.com>
2026-03-13 18:50:29 +0530

    Data Ingestion

---
+++ src/components/data_transformation.py
@@ -0,0 +1,14 @@
+import sys
+from dataclasses import dataclass
+
+@dataclass
+class DataTransformationConfig:
+    pass
+
+
+class DataTransformation:
+    def __init__(self):
+        pass
+
+    def initate_data_transformation(self, train_data, test_data):
+        return train_data, test_data
---
+++ src/components/model_trainer.py
@@ -0,0 +1,15 @@
+import sys
+from dataclasses import dataclass
+
+@dataclass
+class ModelTrainerConfig:
+    pass
+
+
+class ModelTrainer:
+    def __init__(self):
+        pass
+
+    def initate_model_training(self, train_arr, test_arr):
+        print("Model training started")
+        return "Training completed"
